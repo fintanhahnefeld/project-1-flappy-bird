c54f7b570f4aa8d13b936dc276920a1574462b81
Fintan Hahnefeld <fintan22@gmail.com>
2021-04-05 11:14:44 +0200

    add moving background

--- javascript/background.js
+++ javascript/background.js
@@ -7,5 +7,16 @@ class Background {
     draw() {
         // city background
         image(game.backgroundImage, this.imgx, 0, width, height);
+        image(game.backgroundImage, this.imgx +width, 0, width, height);
+
+        // moving city background
+        this.imgx -= 0.2;
+
+        if ( this.imgx <=  -width) {
+            this.imgx = 0;
+        }
+
+        // moving floor
+        image(game.floor, this.floorx, height - 12, width, 12, )
     }
 }
